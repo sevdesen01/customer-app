88cf30d6c760094b1765d9b6716a43f8b36e616e
sevde01 <serifesevdesen1@gmail.com>
2026-06-24 05:35:49 +0300

    first commit

---
+++ src/components/CustomerList/CustomerList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import CustomerItem from '../CustomerItem/CustomerItem';
+import "./CustomerList.css"
+
+
+
+function CustomerList ({ customers, setCustomers}) {
+const handleDelete = (item) => {
+  setCustomers(customers.filter((customer) => customer.id !== item.id));
+};
+  return (
+    <ul className='customer-list'>
+      {customers.map((customer) => (
+        <CustomerItem customer={customer} key={customer.id} handleDelete={handleDelete} />
+      ))}
+    </ul>
+  );
+}
+
+export default CustomerList;
+
+
